7115bc3ea4283f59976faaaede3284ed40d93ade
JoseluisParejo <joseluisparejofernandez@gmail.com>
2014-03-13 13:44:10 +0100

    fin

--- eskup.py
+++ eskup.py
@@ -5,12 +5,13 @@ from lxml import etree
 id = raw_input("¿Cual es tu identificador de usuario? ")
 
 respuesta = requests.get('http://eskup.elpais.com/Outeskup?f=xml&t=t1-%s' % id)
-
+contador = 0
 
 raiz = etree.fromstring(respuesta.text.encode("utf-8"))
 pretty = etree.tostring(raiz, pretty_print = True)
-conten = raiz.find("mensajes/elements/contenido")
+mensajes = raiz.find("mensajes")
 
-if content = 
-print conten.text
+for element in mensajes:
+	conten = element.find("contenido")
+	print conten.text
 
